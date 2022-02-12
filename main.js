c75893472e3d8a0727d3a844d872b82efc5e6224
Diachenko_Ilia <diachenko13qwe@gmail.com>
2022-02-12 18:08:06 +0200

    dz4 exercises

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-var s = window.screen;  //Переменная для выясняющая размер вашего экрана 
-var width = q.width = s.width;
-var height = q.height = s.height;
-var letters = Array(256).join(1).split('');
+// dz-4 branch 
 
-var draw = function () {
-  q.getContext('2d').fillStyle='rgba(0,0,0,.05)'; //Тут цвет фона
-  q.getContext('2d').fillRect(0,0,width,height);
-  q.getContext('2d').fillStyle='#0F0'; //Тут цвет букв
-  letters.map(function(y_pos, index){
-  text = String.fromCharCode(65+Math.random()*33);
-  x_pos = index * 10;
-  q.getContext('2d').fillText(text, x_pos, y_pos);
-  letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
-  });
-};
-setInterval(draw, 33);
+// method .fill
+// как изменить все?
+   let Phones = ['Apple', 'Xiaomi', 'Samsung', 'Huawei',];
+    Phones.fill('Nokia', 2, 3,);
+    console.log(Phones);
+
+// method reverse
+// правильно ли
+  let Fruits = ['Apple', 'Orange', 'Banane'];
+  console.log('fruits:',Fruits);
+  let reversed = Fruits.reverse();
+  console.log('reversed:', reversed);
+  console.log('fruits:' , Fruits);
+
+// 
+const compact = (array) => array.filter((el) => el);
+const data = [0, 1, false, 2, undefined, '', 3, null];
+console.log(compact(data)) // [1, 2, 3]
+
+   
+
+ 
 
