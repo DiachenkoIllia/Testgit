3aec1bd71e05c38748bbc2eb04d7cc1fe58e19b7
Diachenko_Ilia <diachenko13qwe@gmail.com>
2022-02-14 20:16:31 +0200

    array1

--- main.js
+++ main.js
@@ -20,6 +20,22 @@ const data = [0, 1, false, 2, undefined, '', 3, null];
 console.log(compact(data)) // [1, 2, 3]
 
    
+// Добавить значение в массив в определенном порядке ['5', '1', '2', '3', '4', '6']
+//  let array1 = ['1', '2' , '3' , '4'];
+//  array1[4] = '5';
+//  array1[5] = '6';
+//  [array1[4], array1[0]] = [array1[0], array1[4]];
+//  [array1[4], array1[1]] = [array1[1], array1[4]];
+//  [array1[4], array1[2]] = [array1[2], array1[4]];
+//  [array1[4], array1[3]] = [array1[3], array1[4]];
+//  console.log(array1);
 
- 
+// let array2 = ['1', '2' , '3' , '4'];
+// array2[0] = '5';
+// array2[5] = '6';
+// array2[1] = '1';
+// array2[2] = '2';
+// array2[3] = '3';
+// array2[4] = '4';
+// console.log(array2);
 
