068c3976be836682b12bca507c715107b87111e1
Diachenko_Ilia <diachenko13qwe@gmail.com>
2022-03-13 19:30:26 +0200

    testarray

--- main.js
+++ main.js
@@ -84,3 +84,70 @@ test('Ilia');
 // function arrow
 let test2 = name2 => console.log('Hello -', name2);
  test2('Ania');
+
+// Array test
+const cars = ['Mazda', 'Ford', 'BMW', "Audi"]
+const num = [1, 1, 2, 3, 5, 6, 10,]
+// push - add element
+cars.push('Porshe')
+
+cars.unshift('Reno')
+// shift delete first element
+cars.shift()
+const lastCar = cars.pop()
+// console.log(cars);
+
+// const text = 'Hello, we learn JavaScript'
+// const reverseText = text.split('').reverse().join('')
+// console.log(reverseText);
+
+// -------------------------------------------------------------
+
+// findIndex 
+// const people = [
+//   {name: 'Ania', budget: 4500},
+//   {name: "Ilia", budget: 5000},
+//   {name: 'Mia', budget: 228},
+//   {name: 'Lusia', budget: 1488},
+// ]
+// method 
+// const index = people.findIndex(function(person){
+//  return person.budget === 228
+// })
+// console.log(people[index]);
+
+// cycle
+// let findedPerson
+// for (const person of people) {
+//   if (person.budget === 228) {
+//     findedPerson = person
+//   }
+// }
+// console.log(findedPerson)
+
+// pro. cod
+// const person = people.find(person => person.budget === 228)
+// console.log(person);
+// ----------------------------------------------------------------
+// upperCase method
+//  const fruit = ['apple', 'orange', 'pineapple', 'cherry'];
+//  const upperCaseFruit = fruit.map(fruit => {
+//    return fruit.toUpperCase()
+//  })
+// console.log(upperCaseFruit);
+// console.log(fruit); 
+// ----------------------------------------------------------------
+
+const people = [
+    {name: 'Ania', budget: 4500},
+    {name: "Ilia", budget: 5000},
+    {name: 'Mia', budget: 228},
+    {name: 'Lusia', budget: 1488},
+]
+const allBudget = people
+.filter(person => person.budget > 2000)
+.reduce((accum, person) => {
+  accum += person.budget
+  return accum
+}, 0)
+console.log(allBudget);
